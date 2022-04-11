ace5e331d77300bb78ba7971230a099261c5701b
Julin S <julinshaji01@gmail.com>
2022-04-11 11:02:37 +0530

    use spaces instead of tabs

--- z-eyes.sh
+++ z-eyes.sh
@@ -6,24 +6,24 @@ rest_time=20m
 distant_stare_time=20
 
 function show_progress_bar {
-	for i in {0..100..5} # equiv of 20s because 1s per iterations as there are 20 iterations for this loop.
-	do
-		echo $i
-		sleep $(( $distant_stare_time / 20 ))
-	done |
-	zenity --title='z-eyes'  --text='Look 20 feet away for 20 seconds' --progress --auto-close 2> /dev/null
+    for i in {0..100..5} # equiv of 20s because 1s per iterations as there are 20 iterations for this loop.
+    do
+        echo $i
+        sleep $(( $distant_stare_time / 20 ))
+    done |
+    zenity --title='z-eyes'  --text='Look 20 feet away for 20 seconds' --progress --auto-close 2> /dev/null
 
-	if [ "$?" -eq 0 ]	# Didn't cancel
-	then
-		zenity --notification --text "20 seconds is up." 2> /dev/null
-	else				# Cancelled
-		zenity --notification --text "20 seconds was not completed." 2> /dev/null
-	fi
+    if [ "$?" -eq 0 ]    # Didn't cancel
+    then
+        zenity --notification --text "20 seconds is up." 2> /dev/null
+    else                # Cancelled
+        zenity --notification --text "20 seconds was not completed." 2> /dev/null
+    fi
 }
 
 while true
 do
-	sleep "$rest_time" && zenity --info --text "Your eyes are tired. Rest?" --title="z-eyes" 2> /dev/null
+    sleep "$rest_time" && zenity --info --text "Your eyes are tired. Rest?" --title="z-eyes" 2> /dev/null
 
-	show_progress_bar
+    show_progress_bar
 done
